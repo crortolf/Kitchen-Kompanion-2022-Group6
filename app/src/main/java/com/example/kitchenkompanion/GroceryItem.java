3d09f51d5260152c533d280ee78a80d7af98646b
Chris <crortolf@gmail.com>
2022-03-12 14:47:43 -0500

    Created GroceryItem class

---
+++ app/src/main/java/com/example/kitchenkompanion/GroceryItem.java
@@ -0,0 +1,31 @@
+package com.example.kitchenkompanion;
+
+public class GroceryItem {
+    public String name, units;
+    public float amount, minimumAmount;
+
+    public GroceryItem(String name, String units, float amount) {
+        this.name = name;
+        this.units = units;
+        this.amount = amount;
+        minimumAmount = 0;
+    }
+
+    public GroceryItem(String name, String units, float amount, float minimumAmount) {
+        this(name, units, amount);
+        this.minimumAmount = minimumAmount;
+    }
+
+    public String toString() {
+        String amountS, minimumAmountS;
+        amountS = (amount % 1.0 == 0.0) ? String.valueOf((int) amount) : String.valueOf(amount);
+        minimumAmountS = (minimumAmount % 1.0 == 0.0) ? String.valueOf((int) minimumAmount) : String.valueOf(minimumAmount);
+        return amountS + " (min " + minimumAmountS + ") " + units  + "(s) of " + name;
+    }
+
+    public String amountToString() {
+        String amountS;
+        amountS = (amount % 1.0 == 0.0) ? String.valueOf((int) amount) : String.valueOf(amount);
+        return amountS + units;
+    }
+}
--- app/src/main/java/com/example/kitchenkompanion/MainActivity.java
+++ app/src/main/java/com/example/kitchenkompanion/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kitchenkompanion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GroceryItem ex = new GroceryItem("Fruit", "cup", 4);
+        GroceryItem ex2 = new GroceryItem("Veg", "cup", 10, 1);
+
+        TextView topBox = findViewById(R.id.topBox);
+        TextView bottomBox = findViewById(R.id.bottomBox);
+
+        topBox.setText(ex.toString());
+        bottomBox.setText(ex2.toString());
     }
 }
