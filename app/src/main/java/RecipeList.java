13892b23b03dd11a7cab805d63cfb55390699581
Ethan Cronin <ethancronos@gmail.com>
2022-03-18 14:16:07 -0400

    added background and recipe list class

---
+++ app/src/main/java/RecipeList.java
@@ -0,0 +1,12 @@
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeList  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
