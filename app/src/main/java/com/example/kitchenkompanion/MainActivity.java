b9e2ba2ecb450e6b72a137cdc6969da596cfd778
Chris <crortolf@gmail.com>
2022-03-18 20:40:31 -0400

    Homepage progress

--- app/src/main/java/com/example/kitchenkompanion/MainActivity.java
+++ app/src/main/java/com/example/kitchenkompanion/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.kitchenkompanion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,8 +19,18 @@ public class MainActivity extends AppCompatActivity {
 
         TextView topBox = findViewById(R.id.topBox);
         TextView bottomBox = findViewById(R.id.bottomBox);
+        Button recipe = findViewById(R.id.recipeListButton);
 
         topBox.setText(ex.toString());
         bottomBox.setText(ex2.toString());
+
+        View.OnClickListener recipePage = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.recipe_list);
+            }
+        };
+
+        recipe.setOnClickListener(recipePage);
     }
 }
