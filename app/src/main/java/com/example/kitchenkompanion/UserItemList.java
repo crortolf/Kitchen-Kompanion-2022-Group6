857439af8385a61c7cd4f9947af4f0036d810a8b
Chris <crortolf@gmail.com>
2022-04-13 11:45:57 -0400

    Useless lol

--- app/src/main/java/com/example/kitchenkompanion/UserItemList.java
+++ app/src/main/java/com/example/kitchenkompanion/UserItemList.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import org.w3c.dom.Text;
 
@@ -32,8 +33,6 @@ public class UserItemList extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_item_list);
 
-        Typeface mt = getResources().getFont(R.font.helvetica_neue);
-
         spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.user_name_list, android.R.layout.simple_spinner_item);
         spinner.setAdapter(spinnerAdapter);
@@ -50,15 +49,7 @@ public class UserItemList extends AppCompatActivity implements AdapterView.OnIte
         items.add(new GroceryItem("Ramen", "Cups", 16));
 
 
-        UserItemAdapter adapter = new UserItemAdapter(getApplicationContext(), R.layout.adapter_view_user_items, items) {
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-                TextView text = (TextView) view.findViewById(android.R.id.text1);
-                //text.setTypeface(mt);
-                return view;
-            }
-        };
+        UserItemAdapter adapter = new UserItemAdapter(getApplicationContext(), R.layout.adapter_view_user_items, items);
         listView.setAdapter(adapter);
 
         Button recipe = findViewById(R.id.recipesButton);
